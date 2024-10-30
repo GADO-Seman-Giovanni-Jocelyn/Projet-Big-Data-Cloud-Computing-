e3c4cea2813eca6664e7d50cd9b70959b419ac3a
GADO-Seman-Giovanni-Jocelyn <127804497+GADO-Seman-Giovanni-Jocelyn@users.noreply.github.com>
2024-10-30 06:39:41 +0000

    Acceuil.py

--- Dashboard/Acceuil.py
+++ Dashboard/Acceuil.py
@@ -7,7 +7,6 @@ import boto3
 import os
 from dotenv import load_dotenv #dotenv_values
 from pathlib import Path
-import Data
 
 # Charger les variables d'environnement
 #dp = Path(__file__).resolve().parents[2] / '.env'
