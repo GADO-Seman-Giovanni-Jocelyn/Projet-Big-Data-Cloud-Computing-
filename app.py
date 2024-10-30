74be0927b75148fced6c09efa83f92a1c18abd9b
GADO-Seman-Giovanni-Jocelyn <127804497+GADO-Seman-Giovanni-Jocelyn@users.noreply.github.com>
2024-10-30 06:39:02 +0000

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+acc = st.Page(page = "Dashboard/Acceuil.py", title = "Page d"acceuil")
+dat = st.Page(page = "Dashboard/Pages/Data.py", title = "Données")
 
+pg = st.navigation(pages = acc, dat)
+
+pg.run()
